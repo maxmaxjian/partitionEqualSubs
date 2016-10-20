6dffadda53ce3705d41c22635dcb4efbd2f45695
Jian Wei <jianwei@jianwei-deb8-64>
2016-10-20 13:55:11 -0400

    first edit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+
+class solution {
+  public:
+    bool canPartition(const vector<int> & nums) {
+        
+    }
+};
+
+int main() {
+    vector<int> nums{1,5,11,5};
+    solution soln;
+    
+}
